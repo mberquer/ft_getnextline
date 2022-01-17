78a57e2d782d881cc876bf5eca48c5fee6352dca
Maleaume BERQUER <mberquer@e1r13p6.42.fr>
2022-01-17 17:19:22 +0100

    sadness

--- get_next_line.c
+++ get_next_line.c
@@ -6,21 +6,30 @@
 /*   By: mberquer <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/12 11:33:02 by mberquer          #+#    #+#             */
-/*   Updated: 2022/01/12 17:14:13 by mberquer         ###   ########.fr       */
+/*   Updated: 2022/01/17 16:29:38 by mberquer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+char	*ft_read(static char *vault, int fd, char *buf)
+{
+	int		ret;
+	char	*new;
+
+	ret = read(fd, buf, BUFFER_SIZE);
+	buf[ret] = '\0';
+	while (!ft_check(buf))
+
 char	*get_next_line(int fd)
 {
-	static int	count = 0;
-	int		ret;
 	char		*buf;
-	char		*line;
+	static char	*vault;
 
-	while ()
-		ret = read(fd, buf,  BUFFER_SIZE);
-
-	return(line);
+	if (fd < 0 || BUFFER_SIZE  < 0 || count = -1)
+		return (NULL);
+	buf = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buf)	
+		return (NULL);
+	return (ft_read(vault, fd, buf));
 }
