b084e360f63a4b11d33130afedf4f0aceab62f5a
Maleaume <mberquerpro@gmail.com>
2022-01-18 13:25:35 +0100

    ça commence

--- get_next_line.c
+++ get_next_line.c
@@ -12,24 +12,34 @@
 
 #include "get_next_line.h"
 
-char	*ft_read(static char *vault, int fd, char *buf)
+char	*ft_read(static char *vault, int fd, char *buf, int cvault)
 {
 	int		ret;
 	char	*new;
 
-	ret = read(fd, buf, BUFFER_SIZE);
-	buf[ret] = '\0';
-	while (!ft_check(buf))
+    if (cvault == 1)
+        return (ft_get(vault));
+	while (ret = read(fd, buf, BUFFER_SIZE))
+    {
+        buf[ret] = '\0';
+        if (ft_check(buf))
+       {
+            ft_join(vault, ft_save(buf));
+            return (ft_get(ft_join(new, buf)));
+        }
+        ft_join(new, buf);
+    }
+}
 
 char	*get_next_line(int fd)
 {
 	char		*buf;
 	static char	*vault;
 
-	if (fd < 0 || BUFFER_SIZE  < 0 || count = -1)
+	if (fd < 0 || BUFFER_SIZE  < 0)
 		return (NULL);
 	buf = malloc((BUFFER_SIZE + 1) * sizeof(char));
 	if (!buf)	
 		return (NULL);
-	return (ft_read(vault, fd, buf));
+	return (ft_read(vault, fd, buf, ft_cvault(vault)));
 }
