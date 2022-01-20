c5c093191f6ae05df030f16f907d549d5b45ddf8
Maleaume BERQUER <mberquer@e1r13p6.42.fr>
2022-01-20 15:42:46 +0100

    hm

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mberquer <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/12 11:33:02 by mberquer          #+#    #+#             */
-/*   Updated: 2022/01/19 14:55:30 by mberquer         ###   ########.fr       */
+/*   Updated: 2022/01/20 15:42:24 by mberquer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,27 +15,37 @@ char    *ft_join(char *s1, char *s2)
 {
 
 }
-int     ft_cvault(static char vault)
+
+int	ft_check(char *buf)
 {
-    return (1);
+	int	i;
+
+	i = 0;
+	while (buf)
+	{
+		if ((buf[i]) == '\n')
+			return (1);
+	}
+	return (0);
 }
+
 char	*ft_read(static char *vault, int fd, char *buf, int cvault)
 {
 	int		ret;
 	char	*new;
 
-    if (cvault == 1)
-        return (ft_get(vault));
+	if (cvault == 1)
+		return (ft_get(vault));
 	while (ret = read(fd, buf, BUFFER_SIZE))
-    {
-        buf[ret] = '\0';
-        if (ft_check(buf))
-       {
-            ft_join(vault, ft_save(buf));
-            return (ft_get(ft_join(new, buf)));
-        }
-        ft_join(new, buf);
-    }
+	{
+		buf[ret] = '\0';
+		if (ft_check(buf))
+		{
+			ft_join(vault, ft_save(buf));
+			return (ft_get(ft_join(new, buf)));
+		}
+		ft_join(new, buf);
+	}
 }
 
 char	*get_next_line(int fd)
@@ -48,5 +58,5 @@ char	*get_next_line(int fd)
 	buf = malloc((BUFFER_SIZE + 1) * sizeof(char));
 	if (!buf)	
 		return (NULL);
-	return (ft_read(vault, fd, buf, ft_cvault(vault)));
+	return (ft_read(vault, fd, buf, ft_check(vault)));
 }
