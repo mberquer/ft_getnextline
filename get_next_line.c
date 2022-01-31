4ca302a41d4401a70f4fcccf27e63e3bf090049a
Maleaume BERQUER <mberquer@e1r7p16.42.fr>
2022-01-31 13:48:48 +0100

    home time

--- get_next_line.c
+++ get_next_line.c
@@ -6,14 +6,36 @@
 /*   By: mberquer <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/12 11:33:02 by mberquer          #+#    #+#             */
-/*   Updated: 2022/01/20 15:42:24 by mberquer         ###   ########.fr       */
+/*   Updated: 2022/01/26 11:33:10 by mberquer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+size_t	ft_strlen(const char	*s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
 char    *ft_join(char *s1, char *s2)
 {
-
+	char	*s3;
+	
+	if (s1 && s2)
+	{
+		s3 = (char *)malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+		if (!s3)
+			return (NULL);
+		ft_strlcpy(s3, s1, ft_strlen(s1) + 1);
+		ft_strlcat(s3, s2, ft_strlen(s3) + ft_strlen(s2) + 1);
+		return (s3);
+	}
+	return (NULL);
 }
 
 int	ft_check(char *buf)
@@ -21,10 +43,11 @@ int	ft_check(char *buf)
 	int	i;
 
 	i = 0;
-	while (buf)
+	while (buf[i])
 	{
 		if ((buf[i]) == '\n')
 			return (1);
+		i++;
 	}
 	return (0);
 }
@@ -36,16 +59,23 @@ char	*ft_read(static char *vault, int fd, char *buf, int cvault)
 
 	if (cvault == 1)
 		return (ft_get(vault));
-	while (ret = read(fd, buf, BUFFER_SIZE))
+	ret = read(fd, buf, BUFFER_SIZE);
+	buf[ret] = '\0';
+	while (ret)
 	{
-		buf[ret] = '\0';
 		if (ft_check(buf))
 		{
-			ft_join(vault, ft_save(buf));
-			return (ft_get(ft_join(new, buf)));
+			new = ft_join(vault, ft_get(buf));
+			ft_save(vault, buf);
+			return (new);
 		}
-		ft_join(new, buf);
+		ft_save(vault, buf);
+		ret = read(fd, buf, BUFFER_SIZE);
+		buf[ret] = '\0';
 	}
+	new = ft_join(vault, ft_get(buf));
+	ft_save(vault, buf);
+	return (new);
 }
 
 char	*get_next_line(int fd)
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,13 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   gget_next_line_utils.c                             :+:      :+:    :+:   */
+/*   get_next_line_utils.c                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mberquer <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/12 11:30:45 by mberquer          #+#    #+#             */
-/*   Updated: 2022/01/12 11:30:46 by mberquer         ###   ########.fr       */
+/*   Updated: 2022/01/26 12:12:40 by mberquer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
 
+void	ft_save(static char *vault, char *buf);
+{
+	vault = ft_join
+}
+
+char	*ft_get(char *s)
+{
+
+}
