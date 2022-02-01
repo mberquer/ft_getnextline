a306e953e5a88f9427138c418ce2ca1586f03fc6
Maleaume BERQUER <mberquer@e2r13p6.clusters.42paris.fr>
2022-02-01 18:19:36 +0100

    ok leak time

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,13 +6,13 @@
 /*   By: mberquer <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/12 11:30:45 by mberquer          #+#    #+#             */
-/*   Updated: 2022/02/01 16:45:19 by mberquer         ###   ########.fr       */
+/*   Updated: 2022/02/01 17:08:08 by mberquer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*ft_get(char *s)
+char	*ft_get(char **s)
 {
 	char	*s2;
 	char	*temp;
@@ -27,9 +27,15 @@ char	*ft_get(char *s)
 	while (j <= i)
 	{
 		s2[j] = s[j];
+		s[j] = '0';
 		j++;
 	}
 	s2[j] = '\0';
-
+	temp = malloc((ft_strlen(s) - i + 1) * (sizeof(char)));
+	if (!temp)
+		return (NULL);
+	ft_cpy(temp, s);
+	free(s);
+	*s = temp;
 	return (s2);
 }
